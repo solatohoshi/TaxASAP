2afcb8fb2d31247a4e18fdadf96e5f3bfd35edef
loik-b <loikbernatchez@outlook.com>
2025-10-27 13:10:16 -0400

    Implement scroll behavior for bottom bar
    Add scroll event listener to show/hide bottom bar

--- Home_page.js
+++ Home_page.js
@@ -1,1 +1,10 @@
+window.addEventListener("scroll", function() {
+  let bar = document.getElementById("bottomBar");
+
+  if (window.scrollY > 100) {
+    bar.style.bottom = "0";
+  } else {
+    bar.style.bottom = "-250px";
+  }
+});
 
